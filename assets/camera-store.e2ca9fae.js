7ed2f7e3dda414dfba4cd85d86656926b3e5029f
imvenx <imvenx@proton.me>
2025-05-27 21:14:28 +0200

    add

---
+++ assets/camera-store.e2ca9fae.js
@@ -0,0 +1,1 @@
+import{a0 as e}from"./index.052f259a.js";const r=e("cameraStore",{state:()=>({isCameraEnabled:!1})});export{r as u};
--- assets/camera-store.ff6cbfdf.js
+++
@@ -1,1 +0,0 @@
-import{a0 as e}from"./index.f696f9b3.js";const r=e("cameraStore",{state:()=>({isCameraEnabled:!1})});export{r as u};
